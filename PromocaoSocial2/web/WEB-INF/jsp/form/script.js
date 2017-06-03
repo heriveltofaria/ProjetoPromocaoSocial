98c05b1e37d2cc185b425a8f67d627c5594ab6cb
Jonathan <jowjapao@gmail.com>
2017-06-03 19:47:03 -0300

    Novos forms: Usuário ADM, Usuário Assistente Social, Usuário Conselho Tutelar, form CRUD Oficinas, form CRUD Usuários

---
+++ PromocaoSocial2/web/WEB-INF/jsp/form/script.js
@@ -0,0 +1,10 @@
+function listarOficinas(){
+
+	alert("Tem que fazer o script correto para aparecer as tabelas de oficinas ja cadastradas aqui");
+}
+
+
+function listarUsuarios(){
+
+	alert("Tem que fazer o script correto para aparecer as tabelas de oficinas ja cadastradas aqui");
+}
